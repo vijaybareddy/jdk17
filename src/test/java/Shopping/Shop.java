8eb8df5a9cdc3d3c23fd3b8cc8a2cdc075308272
vijaybareddy <vijaybareddy143@gmail.com>
2024-04-05 17:41:55 +0530

    shopping

--- src/test/java/Shopping/Shop.java
+++ src/test/java/Shopping/Shop.java
@@ -6,12 +6,14 @@ public class Shop {
     public List<Product> products;
 
     public Product findProduct(String type, String color, int size) {
-        for (Product product: products) {
-                if(product.type.equals(type)&&product.productSpecfication.color.equals(color)&&product.productSpecfication.size==size){
-                    return product;
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            if (product.type.equals(type) && product.productSpecfication.color.equals(color) && product.productSpecfication.size == size) {
+                return product;
             }
         }
         return null;
     }
 }
+
 
